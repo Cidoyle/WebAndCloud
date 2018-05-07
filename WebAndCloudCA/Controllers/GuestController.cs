926081b35dedf660f0658a19026fd211ed396bb3
Charlene Reilly <charlener83@hotmail.com>
2018-05-07 15:46:49 +0100

    Added Login Page. Adjusted Registration Page

--- WebAndCloudCA/Controllers/GuestController.cs
+++ WebAndCloudCA/Controllers/GuestController.cs
@@ -15,12 +15,44 @@ namespace WebAndCloudCA.Controllers
         {
             return View();
         }
-    }
 
-    //[HttpPost]
-    //[ValidateAntiForgeryToken]
-    //public ActionResult Registration(Guest guest)
-    //{
-    //    return View(guest);
-    //}
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Registration(Guest guest)
+        {
+            //NOTE: Conmplete when Database is created
+            //if (ModelState.IsValid)
+            //{
+            //    using (Database db)
+            //    {
+            //        db.Guest.Add(guest);
+            //        db.SaveChanges();
+            //        ModelState.Clear();
+            //        guest=null;
+            //        ViewBag.Message= "Registration Successful."
+            //    }
+            //}
+            return View(guest);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login(Login login)
+        {
+            return View(login);
+        }
+
+        [Authorize]
+        public  ActionResult Logout()
+        {
+            //Add code 
+            return RedirectToAction("Login", "Guest");
+        }
+    }
 }
---
+++ WebAndCloudCA/Models/Login.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebAndCloudCA.Models
+{
+    public class Login
+    {
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Valid Email Address must be entered.")]
+        public string Email { get; set; }
+
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Valid Password must be entered.")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
