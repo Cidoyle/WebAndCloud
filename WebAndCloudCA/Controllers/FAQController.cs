2e08a3a30f84f0bcaf55621b65e96dff532e3ff2
Pszlachet <10063652@mydbs.ie>
2018-05-26 23:27:24 +0100

    Added FAQ view and controller with Xml DB

---
+++ WebAndCloudCA/Controllers/FAQController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Xml;
+
+namespace WebAndCloudCA.Controllers
+{
+    public class FAQController : Controller
+    {
+        // GET: FAQ
+        public ActionResult Index()
+        {
+            //string url = "~/App_Data/FAQ.xml";
+            XmlDocument xdoc = new XmlDocument(); // creating an instance of XmlDocument
+            xdoc.Load(HttpContext.Server.MapPath("~/App_Data/FAQ.xml"));
+            //xdoc.Load(url); // loading XML from the url
+            XmlNode root = xdoc.DocumentElement;// gives the root node
+            XmlNode channel = root.FirstChild; //First child of rss (root)
+
+            //XmlNodeList list = channel.ChildNodes;
+
+            //using XPath expression
+            XmlNodeList list = xdoc.SelectNodes("/faq/item");
+            //XmlNodeList list = xdoc.SelectNodes("/faq/item[position()<=10]");
+            ViewBag.List = list;
+            return View();
+        }
+    }
+}
--- WebAndCloudCA/Scripts/MyJavaScript.js
+++ WebAndCloudCA/Scripts/MyJavaScript.js
@@ -1,14 +1,14 @@
-﻿function validateEmail() {
-    var email = document.getElementById("emailVal").value;
-    var emailValid = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/;
-    if (!(email).match(emailValid)) {
-        alert("Invalid Email...!!!!!!");
-        return false;
-    }
-    else {
-        return true;
-    }
-}
+﻿//function validateEmail() {
+//    var email = document.getElementById("emailVal").value;
+//    var emailValid = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/;
+//    if (!(email).match(emailValid)) {
+//        alert("Invalid Email...!!!!!!");
+//        return false;
+//    }
+//    else {
+//        return true;
+//    }
+//}
 
 
 $(document).ready(function ()
@@ -19,3 +19,36 @@ setInterval(function()
     $('#slideShowImg > div:first').fadeOut(0).next().fadeIn(1500).end().appendTo('#slideShowImg');
 }, 4000);
 });
+
+
+function enableAnswers() {
+    hideAnswers();
+    var questions = document.getElementsByClassName("question");
+    for (var i = 0; i < questions.length; i++) {
+        questions[i].onclick = function () {
+            var answer = this.nextElementSibling;
+            if (isVisible(answer)) {
+                answer.style.display = "none";
+            }
+            else {
+                answer.style.display = "block";
+            }
+        }
+    }
+}
+function hideAnswers() {
+    var questions = document.getElementsByClassName("question");
+    for (var i = 0; i < questions.length; i++) {
+        var answer = questions[i].nextElementSibling;
+        if (isVisible(answer)) {
+            answer.style.display = "none";
+        }
+    }
+}
+function isVisible(element) {
+    var result = false;
+    if (element.style.display != "none") {
+        result = true;
+    }
+    return result;
+}
